a1b4155dfc3c4a86d932c869d066b32d59c03ae9
ArseniyKotov <kotov.a.arseniy@gmail.com>
2017-05-03 14:57:57 -0700

    added change

--- server/models/queryHelper.js
+++ server/models/queryHelper.js
@@ -5,7 +5,7 @@ var asyncMap = function(tasks, cb) {
   let remaining = tasks.length;
   tasks.forEach((task, i) => {
     task((result) => {
-      results[i] = result;
+      results.push(result);
       remaining -= 1;
       if (remaining === 0) {
         cb(results);
@@ -15,7 +15,11 @@ var asyncMap = function(tasks, cb) {
 };  
 
 var querySchoolTable = function(column, value, cb) {
-  connection.query('SELECT * FROM Universities WHERE ? = ?', [column, value], function(err, results, fields) {
+  console.log('column', column);
+  console.log('value', value);
+  var options = [column, value];
+  connection.query('SELECT * FROM Universities WHERE ? = ?', options, function(err, results, fields) {
+    console.log('MY RESULTS', results);
     if (err) {
       cb(err, null);
     } else {
@@ -30,16 +34,20 @@ var mySearchFunction = function(prefs, cb) {
     queries.push(function(cb) {
       querySchoolTable(key, prefs[key], function(err, data) {
         if (err) {
+          console.log('IM ERRORING', err);
           cb(err, null);
         } else {
+          console.log('MY RESULTS', data);
           cb(null, data);
         }
       });
     });
   }
+  console.log('THE QUERIES', queries);
   asyncMap(queries, function(arrOfData) {
     // and here arrOfData is an array of arrays, the inner arrays represent the result of querying the table for a given column/value pair (like STATE='CA' or MAJOR='Computer Science' or whatever);
     // now you can just sort it using whatever method you want
+    console.log('THE QUERIES INSIDE ASYNCMAP', queries);
     console.log('THE RESULT', arrOfData);
   });
 };
