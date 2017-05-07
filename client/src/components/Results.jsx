bd1e553ba72d4652a5543276c88f8798c2fafa84
Farrah Bousetta <farrah.bousetta@gmail.com>
2017-05-07 10:31:52 -0700

    added functionality to Results component

--- client/src/components/Results.jsx
+++ client/src/components/Results.jsx
@@ -1,16 +1,39 @@
 import React from 'react';
 import ResultsList from './ResultsList.jsx';
+var _ = require('lodash');
 
 class Results extends React.Component {
   constructor(props) {
     super(props);
+
+  this.state = {
+    colleges: []
   }
+
+  componentDidMount: function() {
+
+  }
+
+  componentWillMount: function() {
+
+  }
+
+  _onChange: function() {
+    this.setState({
+      colleges: colleges.getSuggestions()
+    })
+  }
+
+
+}
+
 
   render() {
     return (
        <div>
-        <h5>Results</h5>
-        <ResultsList />
+        {this.state.colleges.map((college, i) => {
+          return <ResultsList key={i} college ={college} />
+        })}
       </div>
     );
   }
