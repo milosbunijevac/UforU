54b227ac0260fa11ade680dcbbd4efd956df4ecd
Farrah Bousetta <farrah.bousetta@gmail.com>
2017-05-07 22:31:15 -0700

    ability to pass user prefs to results

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,19 +4,35 @@ import $ from 'jquery';
 import Survey from './components/Survey.jsx';
 import Results from './components/Results.jsx';
 
-
 class App extends React.Component {
   constructor(props) {
+  
     super(props);
-
+    this.state = {prefs: null}
+    this.onChildChanged = this.onChildChanged.bind(this)
   }
+
+  onChildChanged(newState){
+    console.log('inside onChildChanged, state passed:',newState)
+    console.log('inside onChildChanged, this is:', this)
+    console.log('inside onChildChanged before update state: ',this.state)
+    this.setState({prefs: newState})
+    // setInterval(x => console.log(this.state),5000)
+    console.log('inside onChildChanged after update state: ',this.state)
+  }
+
+
+
+
+
+
 
   render() {
     return (
       <div>
         <h1>UForU</h1>
-        <Survey />
-        <Results/>
+        <Survey callbackParent = {this.onChildChanged} />
+        <Results prefs = {this.state.prefs} />
       </div>
     );
   }
