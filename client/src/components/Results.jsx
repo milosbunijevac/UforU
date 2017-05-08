42dd477123cee993e636869af7c0a5ffc55ab1c2
Farrah Bousetta <farrah.bousetta@gmail.com>
2017-05-07 22:30:33 -0700

    updated state management/added ajax for POST

--- client/src/components/Results.jsx
+++ client/src/components/Results.jsx
@@ -1,43 +1,40 @@
 import React from 'react';
 import ResultsList from './ResultsList.jsx';
 var _ = require('lodash');
+import $ from 'jquery';
 
 class Results extends React.Component {
   constructor(props) {
+    console.log('props inside results: ',props)
     super(props);
 
   this.state = {
     colleges: []
   }
-
-  componentDidMount: function() {
-
-  }
-
-  componentWillMount: function() {
-
-  }
-
-  _onChange: function() {
-    this.setState({
-      colleges: colleges.getSuggestions()
+}
+ componentDidMount() {
+  // console.log('this inside didMount: ', this)
+  console.log('props inside didMount: ',this.props)
+    $.ajax({
+      type: 'POST',
+      url: '/api/colleges/suggestions',
+      data: this.props.prefs
+    })
+    .done(function(res){
+      this.setState({colleges:res.data})
+      console.log(data)
+    })
+    .fail(function(err){
+      console.log('error: ',err)
     })
   }
-
-
-}
-
 
   render() {
     return (
        <div>
-        {this.state.colleges.map((college, i) => {
-          return <ResultsList key={i} college ={college} />
-        })}
+       {this.state.colleges}
       </div>
     );
   }
-
 }
-
 export default Results;
