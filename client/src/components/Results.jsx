94d894af8236c3225627b984cfe58143e7f7fd0d
Farrah Bousetta <farrah.bousetta@gmail.com>
2017-05-08 12:24:53 -0700

    another attempt at the fixxx

--- client/src/components/Results.jsx
+++ client/src/components/Results.jsx
@@ -5,36 +5,15 @@ import $ from 'jquery';
 
 class Results extends React.Component {
   constructor(props) {
-    console.log('props inside results: ',props)
     super(props);
 
-  this.state = {
-    colleges: []
   }
-}
- componentDidMount() {
-  // console.log('this inside didMount: ', this)
-  console.log('props inside didMount: ',this.props)
-    $.ajax({
-      type: 'POST',
-      url: '/api/colleges/suggestions',
-      data: this.props.prefs
-    })
-    .done(function(res){
-      this.setState({colleges:res.data})
-      console.log(data)
-    })
-    .fail(function(err){
-      console.log('error: ',err)
-    })
-  }
+
 
   render() {
     return (
-       <div>
-       {this.state.colleges}
-      </div>
-    );
   }
 }
+
+
 export default Results;
--- client/src/components/ResultsList.jsx
+++ client/src/components/ResultsList.jsx
@@ -6,14 +6,7 @@ class ResultsList extends React.Component {
   }
 
   render () {
-    const college = this.props.college;
-    return (
-    <div>
-      <h3>{college.name} </h3>
-      <div>{college.description}</div>
-      <img src={college.image_url}/>
-    </div>
-    );
+
   }
 
 }
