324cfa9d7cde99af6ae89bc1981c9a6d3ff487a0
Farrah Bousetta <farrah.bousetta@gmail.com>
2017-05-08 18:37:04 -0700

    mapping over colleges

--- client/src/components/ResultsList.jsx
+++ client/src/components/ResultsList.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
+import ResultListEntry from './ResultListEntry.jsx';
 
 class ResultsList extends React.Component {
-  constructor (props) {
+  constructor(props) {
     super(props);
-  }
-
-  render () {
 
   }
 
+
+  render() {
+    return (
+      <div>
+        {this.props.colleges.map((college, i) => {
+          return <ResultListEntry key={i} college={college} />
+        })}
+      </div>
+    );
+  }
 }
 
 export default ResultsList;
-
-
