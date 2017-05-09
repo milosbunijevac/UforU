8b48b9eed39d26f22f5929c7c5a732616b3e2b18
Farrah Bousetta <farrah.bousetta@gmail.com>
2017-05-08 21:43:46 -0700

    results now rendering results

--- client/src/components/Results.jsx
+++ client/src/components/Results.jsx
@@ -1,44 +1,19 @@
 import React from 'react';
-import ResultsList from './ResultsList.jsx';
 import $ from 'jquery';
+import _ from 'lodash';
 
 class Results extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      colleges: []
-    }
   }
 
-
-componentDidMount() {
-   $.ajax({
-      url: '/api/colleges/',
-      method: 'GET',
-      success: (data) => {
-        this.setState({
-          colleges: data
-          
-        });
-      },
-      error: (err) => {
-        console.log('Some Error:', err);
-      }
-    });
-
-}
-
-
-
-
-   
-
-
   render() {
+    var colleges = this.props.colleges
+
     return (
       <div>
-      <ResultsList colleges={this.state.colleges}/>
+      {JSON.stringify(colleges)}
       </div>
     );
   }
