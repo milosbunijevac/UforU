46ca8dae2ec6bde6396620eaac231efcd06bd98c
ArseniyKotov <kotov.a.arseniy@gmail.com>
2017-05-15 08:49:55 -0700

    removed oldschool connection

--- server/models/dbConnection.js
+++ server/models/dbConnection.js
@@ -2,6 +2,6 @@ var mysql = require('mysql');
 
 var connection = mysql.createPool(process.env.CLEARDB_DATABASE_URL || {host: '127.0.0.1', user: 'root', password: '', database: 'UFORU'});
 
-connection.connect();
+// connection.connect();
 
 module.exports.connection = connection;
