e7c5d20000e566336c46ecf46f9258f260cdb714
Milos Bunijevac <milos.bunijevac@gmail.com>
2017-05-16 18:25:16 -0700

    "login page with axios post call to /login and two handleclicks"

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,8 +1,34 @@
 import React from 'react';
+import axios from 'axios';
 
 class Login extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {username: '', password: ''};
+  }
+
+  onLoginSubmit () {
+    let userInfo = {username: this.state.username, password: this.state.password};
+    axios({
+      url: '/login',
+      method: 'POST',
+      data: userInfo,
+    })
+      .then (
+        console.log('Login submission posted')
+      )
+      .catch ((error) => {
+        console.log('An error occurred inside of the onLoginSubmit method' + error);
+      });
+  }
+
+  handleClickUser (event) {
+    this.setState({username: event.target.value});
+  }
+
+  handleClickPass (event) {
+    this.setState({password: event.target.value});
   }
 
 
@@ -11,12 +37,12 @@ class Login extends React.Component {
       <div>
         <h3 className = "loginText"> Please Login below </h3>
         <h3 className = "loginText"> Logging in will allow you to save and customize various preferences including colleges, tuition and more! </h3>
-        <div className = "inputLogin col-md-12">
-          <input type = "text" name = "username" defaultValue = "Enter Username"></input>
-          <input type = "text" name = "password" defaultValue = "Enter Password"></input>
+        <div className = "col-md-6 col-md-offset-3">
+          <input className = "inputText" type = "text" name = "username" defaultValue = "Enter Username" onChange={this.handleClickUser.bind(this)}></input>
+          <input className = "inputText" type = "text" name = "password" defaultValue = "Enter Password" onChange={this.handleClickPass.bind(this)}></input>
         </div>
         <div className = "loginButton">
-          <button type = "submit">Login</button>
+          <button className = "loginButton" type = "submit" onClick = {this.onLoginSubmit.bind(this)}>Login</button>
         </div>
           
       </div>
