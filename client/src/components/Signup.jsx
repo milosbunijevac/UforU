960820872f917ef464a5e39b88745dc28f4247f2
Rishi Raje <hrishiraje@gmail.com>
2017-05-16 17:26:46 -0700

    Capturing username and password from signup page

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -3,17 +3,38 @@ import React from 'react';
 class Signup extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      username: '',
+      password: ''
+    };
   }
 
+usernameHandler(e) {
+  this.setState({
+    username: e.target.value
+  });
+}
+
+passwordHandler(e) {
+  this.setState({
+    password: e.target.value
+  });
+}
+
+submitHandler() {
+  console.log('sending ', this.state.username, this.state.password);
+}
 
   render() {
     return (
       <div>
         <h2> Hello from signup </h2>
-        {/*<form method = "POST">
-          <input type = "text" name = "username" defaultValue = "Enter Username"></input>
-          <input type = "text" name = "password" defaultValue = "Enter Password"></input>
-        </form>*/}
+        
+  <input type="text" name="username" onChange = {this.usernameHandler.bind(this)} />
+  <input type="text" name="password" onChange = {this.passwordHandler.bind(this)}/>
+  <input type="submit" onClick = {this.submitHandler.bind(this)}/>
+
+
       </div>
     );
   }
--- server/router/router.js
+++ server/router/router.js
@@ -3,6 +3,10 @@ var controllers = require('../controllers/controllers.js');
 
 router.get('/api/colleges', controllers.colleges.get);
 
+router.post('/signup', function(){
+  console.log('server got the route for signup');
+})
+
 router.post('/api/colleges/suggestions', controllers.colleges.getSuggestions);
 
 module.exports = router;
