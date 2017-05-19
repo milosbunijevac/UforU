3c21b93d79f1b17b1cb17c05212c9b45e42955bd
jen1425 <jenniferhwa@gmail.com>
2017-05-18 17:52:04 -0700

    Fix merge conflict

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -14,21 +14,13 @@ class Nav extends React.Component {
       <nav className="navbar navbar-inverse">
         <div className="container-fluid" className="navigation">
           <div className="navbar-header">
-            {/*<a href="#" className="navbar-brand">UforU</a>*/}
           </div>
           <div className="collapse navbar-collapse">
               <ul className="nav navbar-nav">
                 <li><Link to="/favorites">Favorites</Link></li>
               </ul>
               <ul className="nav navbar-nav navbar-right">
-                </ul>
-              <ul className="nav navbar-nav navbar-right">
                   <li><Link to="/logout">Logout</Link></li>
-              </ul>
-              <ul className="nav navbar-nav navbar-right">
-                  <li className="dropdown">
-                    <a href="#" className="dropdown-toggle" role="button">Settings <span className="caret"></span></a>
-                  </li>
               </ul>
               </div>
           </div>
