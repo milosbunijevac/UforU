5d15e1c7b787995c26dd52d0f73360f366a10626
Milos Bunijevac <milos.bunijevac@gmail.com>
2017-05-18 19:32:40 -0700

    "got the favorites button to work and delete off the favorites page"

--- client/src/components/FavsListEntry.jsx
+++ client/src/components/FavsListEntry.jsx
@@ -27,6 +27,22 @@ class FavsListEntry extends React.Component {
   //   });
   // }
 
+  deleteHandler(entry) {
+    console.log('delete handler called');
+    // axios({
+    //   url: '/favoritesRemove',
+    //   method: 'POST',
+    //   data: {collegeId: entry.id}
+    // })
+    // .then((results) => {
+    //   console.log('deleted from favorites - message recieved by client from server');
+    // })
+    // .catch((error) => {
+    //   console.log('error deleting a favorite');
+    // }); 
+    this.props.deleteHandler(entry);
+  }
+
   render () {
     const college = this.props.college;
     return (
@@ -35,9 +51,10 @@ class FavsListEntry extends React.Component {
           <div className="col-md-3">
             <img className="img-responsive cardImages style_prevu_kit" src = {college.image_url}/>
           </div>
-          <a className="college-name" href={"http://" + college.website_url}> {college.name}</a>
+          <a className="college-name" href={'http://' + college.website_url}> {college.name}</a>
           {/*<div className="heart" onClick={this.favoriteHandler.bind(this,college)}></div>*/}
           {/*<p>{this.state.showMessage? 'College added to your favorites!': ''}</p>*/}
+          <button className="deleteFavs" onClick={this.deleteHandler.bind(this, college)}>Remove from Favorites</button>
           <p className="description">{college.description}</p>
         </div>
       </div>
